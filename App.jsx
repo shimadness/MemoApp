4c72241cd510b29418419ddadce857106173bc59
shimadness <shimadness@gmail.com>
2021-08-08 12:09:14 +0900

    install eslint

--- App.jsx
+++ App.jsx
@@ -7,6 +7,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Hoge Hoge Dane!!!</Text>
+      {</* eslint-disable */>}
       <StatusBar style="auto" />
     </View>
   );
